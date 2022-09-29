a0be3f6fdf2d00d917b25c648b9a821e73ecd0e9
Jose10C <c2-jose@hotmail.com>
2022-09-28 22:58:29 -0500

    subida del proyecto

---
+++ app/Views/webCliente/contacto.php
@@ -0,0 +1,117 @@
+<?php echo $cabecera ?>
+
+
+<!-- Breadcrumbs -->
+<div class="breadcrumbs">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="bread-inner">
+                    <ul class="bread-list">
+                        <li><a href="index1.html">Inicio<i class="ti-arrow-right"></i></a></li>
+                        <li class="active"><a href="blog-single.html">Contactanos</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Breadcrumbs -->
+
+<!-- Start Contact -->
+<section id="contact-us" class="contact-us section">
+    <div class="container">
+        <div class="contact-head">
+            <div class="row">
+                <div class="col-lg-8 col-12">
+                    <div class="form-main">
+                        <div class="title">
+                            <h4>Ponerse en Contacto</h4>
+                            <h3>Escribir Mensaje</h3>
+                        </div>
+                        <form class="form" method="post" action="mail/mail.php">
+                            <div class="row">
+                                <div class="col-lg-6 col-12">
+                                    <div class="form-group">
+                                        <label>Nombres<span>*</span></label>
+                                        <input name="name" type="text" placeholder="">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-12">
+                                    <div class="form-group">
+                                        <label>Apellidos<span>*</span></label>
+                                        <input name="subject" type="text" placeholder="">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-12">
+                                    <div class="form-group">
+                                        <label>Correo Electronico<span>*</span></label>
+                                        <input name="email" type="email" placeholder="">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-12">
+                                    <div class="form-group">
+                                        <label>Telefono<span>*</span></label>
+                                        <input name="company_name" type="text" placeholder="">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group message">
+                                        <label>Mensaje<span>*</span></label>
+                                        <textarea name="message" placeholder=""></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group button">
+                                        <button type="submit" class="btn ">Enviar Mensaje</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="single-head">
+                        <div class="single-info">
+                            <i class="fa fa-phone"></i>
+                            <h4 class="title">Telefono de contacto:</h4>
+                            <ul>
+                                <li><?php echo $item['telefono1_empresa'] ?></li>
+                                <li><?php echo $item['telefono1_empresa'] ?></li>
+                            </ul>
+                        </div>
+                        <div class="single-info">
+                            <i class="fa fa-envelope-open"></i>
+                            <h4 class="title">Email:</h4>
+                            <ul>
+                                <li><a href="mailto:jaimecarrascotaipe@gmail.com"><?php echo $item['email1_empresa'] ?></a></li>
+                                <li><a href="mailto:heladeria_ice_cream@gmail.com"><?php echo $item['email2_empresa'] ?></a></li>
+                            </ul>
+                        </div>
+                        <div class="single-info">
+                            <i class="fa fa-location-arrow"></i>
+                            <h4 class="title">Direccion:</h4>
+                            <ul>
+                                <li><?php echo $item['ubicacion_empresa'] ?></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--/ End Contact -->
+
+<!-- Map Section -->
+
+<!--/ End Map Section -->
+
+<div class="row">
+    <div class="col-12" style="background-color: white;">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7981.954261233849!2d-72.87672005798495!3d-13.635738278476959!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9245fcc955209068!2zMTPCsDM4JzA5LjkiUyA3MsKwNTInMTguMiJX!5e0!3m2!1ses!2spe!4v1657251654555!5m2!1ses!2spe" width="100%" height="500px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</div>
+
+
+<?php echo $pie ?>
---
+++ app/Views/webCliente/nosotros.php
@@ -0,0 +1,232 @@
+<?php echo $cabecera ?>
+<!-- Breadcrumbs -->
+<div class="breadcrumbs">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="bread-inner">
+                    <ul class="bread-list">
+                        <li><a href="index1.html">Inicio<i class="ti-arrow-right"></i></a></li>
+                        <li class="active"><a href="blog-single.html">Sobre nosotros</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Breadcrumbs -->
+
+<!-- About Us -->
+<section class="about-us section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-12">
+                <div class="about-content">
+                    <h3>Bienvenidos a <span>IceCream Abancay</span></h3>
+                    <p>En IceCream buscamos acercarte más momentos felices con nuestras exquisitas cremas heladas, helados de fruta y mucho mas elaboradas con materia prima de la mejor calidad. </p>
+                    <p>Nuestra atencion y servicio son de la mas alta calidad y contamos con certificados otorgados por la NASA y la ONU</p>
+                    <p>Ubicanos en: Av.Garcilazo con Huancavelica N.123</p>
+                    <div class="button">
+                        <a href="blog.html" class="btn">Nuestro Blog</a>
+                        <a href="contact.html" class="btn primary">Contactanos</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-12">
+                <div class="about-img overlay">
+                
+                    <img src="https://i.pinimg.com/originals/cb/79/5a/cb795a72975c668ce8ae81da79596c8f.jpg" alt="#">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End About Us -->
+
+
+<!-- Start Team -->
+<section id="team" class="team section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title">
+                    <h2>NUESTRO EQUIPO</h2>
+                    <p>Conozca a nuestros expertos en brindarle la mejor experiencia</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <!-- Single Team -->
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="single-team">
+                    <!-- Image -->
+                    <div class="image">
+                        <img src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="#">
+                    </div>
+                    <!-- End Image -->
+                    <div class="info-head">
+                        <!-- Info Box -->
+                        <div class="info-box">
+                            <h4 class="name"><a href="#">Jaime Ivan Carrasco Taipe</a></h4>
+                            <span class="designation">Gerente General</span>
+                        </div>
+                        <!-- End Info Box -->
+                        <!-- Social -->
+                        <div class="social-links">
+                            <ul class="social">
+                                <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                        <!-- End Social -->
+                    </div>
+                </div>
+            </div>
+            <!-- End Single Team -->
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="single-team">
+                    <!-- Image -->
+                    <div class="image">
+                        <img src="https://www.fasdapsicanalise.com.br/content/uploads/2020/08/A-CI%C3%8ANCIA-REVELA-O-QUE-A-FOTO-DO-SEU-PERFIL-DIZ-SOBRE-A-SUA-PERSONALIDADE.jpg" alt="#">
+                    </div>
+                    <!-- End Image -->
+                    <div class="info-head">
+                        <!-- Info Box -->
+                        <div class="info-box">
+                            <h4 class="name"><a href="#">Cintia Pareja Arredondo</a></h4>
+                            <span class="designation">Jefa de Ventas</span>
+                        </div>
+                        <!-- End Info Box -->
+                        <!-- Social -->
+                        <div class="social-links">
+                            <ul class="social">
+                                <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                        <!-- End Social -->
+                    </div>
+                </div>
+            </div>
+                        <!-- End Single Team -->
+                        <div class="col-lg-3 col-md-6 col-12">
+                <div class="single-team">
+                    <!-- Image -->
+                    <div class="image">
+                        <img src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="#">
+                    </div>
+                    <!-- End Image -->
+                    <div class="info-head">
+                        <!-- Info Box -->
+                        <div class="info-box">
+                            <h4 class="name"><a href="#">Alonzo Huamani Sanchez</a></h4>
+                            <span class="designation">Cajero</span>
+                        </div>
+                        <!-- End Info Box -->
+                        <!-- Social -->
+                        <div class="social-links">
+                            <ul class="social">
+                                <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                        <!-- End Social -->
+                    </div>
+                </div>
+            </div>
+                        <!-- End Single Team -->
+                        <div class="col-lg-3 col-md-6 col-12">
+                <div class="single-team">
+                    <!-- Image -->
+                    <div class="image">
+                        <img src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="#">
+                    </div>
+                    <!-- End Image -->
+                    <div class="info-head">
+                        <!-- Info Box -->
+                        <div class="info-box">
+                            <h4 class="name"><a href="#">Jose Condori Condori</a></h4>
+                            <span class="designation">Vendedor</span>
+                        </div>
+                        <!-- End Info Box -->
+                        <!-- Social -->
+                        <div class="social-links">
+                            <ul class="social">
+                                <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                        <!-- End Social -->
+                    </div>
+                </div>
+            </div>
+            <!-- End Single Team -->
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="single-team">
+                    <!-- Image -->
+                    <div class="image">
+                        <img src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="#">
+                    </div>
+                    <!-- End Image -->
+                    <div class="info-head">
+                        <!-- Info Box -->
+                        <div class="info-box">
+                            <h4 class="name"><a href="#">Jhon Franklin Romero Ramos</a></h4>
+                            <span class="designation">Vendedor</span>
+                        </div>
+                        <!-- End Info Box -->
+                        <!-- Social -->
+                        <div class="social-links">
+                            <ul class="social">
+                                <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                        <!-- End Social -->
+                    </div>
+                </div>
+            </div>
+            <!-- End Single Team -->
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="single-team">
+                    <!-- Image -->
+                    <div class="image">
+                        <img src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="#">
+                    </div>
+                    <!-- End Image -->
+                    <div class="info-head">
+                        <!-- Info Box -->
+                        <div class="info-box">
+                            <h4 class="name"><a href="#">Anderson Ramos Hurtado</a></h4>
+                            <span class="designation">Seguridad</span>
+                        </div>
+                        <!-- End Info Box -->
+                        <!-- Social -->
+                        <div class="social-links">
+                            <ul class="social">
+                                <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                        <!-- End Social -->
+                    </div>
+                </div>
+            </div>
+            <!-- End Single Team -->
+        </div>
+    </div>
+</section>
+<!--/ End Team Area -->
+
+
+
+
+
+
+<?php echo $pie ?>
